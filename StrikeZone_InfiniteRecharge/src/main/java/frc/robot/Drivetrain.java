157a688829c61c86f07868ec93836658e80d1cfd
Tomminator125 <52144539+Tomminator125@users.noreply.github.com>
2020-01-05 13:27:58 -0500

    Added Basic Drivetrain Code

---
+++ StrikeZone_InfiniteRecharge/src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+/**
+ * Add your docs here.
+ */
+public class Drivetrain {
+    TalonSRX LeftDriveM = new TalonSRX(1);
+    VictorSPX LeftDriveS1 = new VictorSPX(2);
+    VictorSPX LeftDriveS2 = new VictorSPX(3);
+    TalonSRX RightDriveM = new TalonSRX(4);
+    VictorSPX RightDriveS1 = new VictorSPX(5);
+    VictorSPX RightDriveS2 = new VictorSPX(6);
+     
+   
+
+
+
+
+
+}
