c8ae5e4242a2c0dad28cdbd2c0c6b5d3532bb2e4
schneidermatthew97 <59298114+schneidermatthew97@users.noreply.github.com>
2020-02-08 16:03:29 -0500

    Bella's Hopper Logic

--- StrikeZone_InfiniteRecharge/src/main/java/frc/robot/Subsystems/Hopper.java
+++ StrikeZone_InfiniteRecharge/src/main/java/frc/robot/Subsystems/Hopper.java
@@ -7,7 +7,10 @@
 
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
 
 /**
@@ -16,11 +19,62 @@ import edu.wpi.first.wpilibj.Solenoid;
 public class Hopper extends RobotMap{
     Solenoid ballStop = new Solenoid(5);
 
-    AnalogInput ballPos1 = new AnalogInput(0);
-    AnalogInput ballPos2 = new AnalogInput(1);
-    AnalogInput ballPos3 = new AnalogInput(2);
-    AnalogInput ballPos4 = new AnalogInput(3);
+    DigitalInput ball0 = new DigitalInput(0);
+    DigitalInput ball1 = new DigitalInput(1);
+    DigitalInput ball2 = new DigitalInput(2);
 
+    public void hopperLogic(){
+        int theState = (ball0.get()?0:1)+(ball1.get()?0:2)+(ball2.get()?0:4);
+    switch (theState){
+      // Might need to apply delays to motors stopping, to accomodate the balls moving from position to position
+      case 0:
+      //no balls in hopper
+        hopperHorizontal.set(ControlMode.PercentOutput, 0.5);
+        hopperVertical.set(ControlMode.PercentOutput, 0);
+        break;
+      case 1:
+      // ball in horizontal position, no where else
+        hopperHorizontal.set(ControlMode.PercentOutput, 0.5);
+        hopperVertical.set(ControlMode.PercentOutput, 0);
+        break;
+      case 2:
+      // ball in bottom vertical position, no where else
+      // make it at some point that there's a delay before this activates, in case a ball is coming
+        hopperHorizontal.set(ControlMode.PercentOutput, 0);
+        hopperVertical.set(ControlMode.PercentOutput, 0.5);
+        break;
+      case 3:
+      // balls in horizontal and bottom vertical positions
+        hopperHorizontal.set(ControlMode.PercentOutput, 0);
+        hopperVertical.set(ControlMode.PercentOutput, 0.5);
+        break;
+      case 4:
+      // ball in top vertical position
+        hopperHorizontal.set(ControlMode.PercentOutput, 0.5);
+        hopperVertical.set(ControlMode.PercentOutput, 0);
+        break;
+      case 5:
+      // balls in top vertical and horizontal positions
+        hopperHorizontal.set(ControlMode.PercentOutput, 0.5);
+        hopperVertical.set(ControlMode.PercentOutput, 0);
+        break;
+      case 6:
+      // balls in top and bottom vertical positions
+        hopperHorizontal.set(ControlMode.PercentOutput, 0.5);
+        hopperVertical.set(ControlMode.PercentOutput, 0);
+        break;
+      case 7:
+      // full hopper
+        hopperHorizontal.set(ControlMode.PercentOutput, 0);
+        hopperVertical.set(ControlMode.PercentOutput, 0);
+        break;
+
+
+
+    }
+
+
+    }
 
 
 }
