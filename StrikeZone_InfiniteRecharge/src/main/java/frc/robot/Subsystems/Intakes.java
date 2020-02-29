dab4d0800f2a298d23127a510a137278ffb0cdd1
Tomminator125 <52144539+Tomminator125@users.noreply.github.com>
2020-02-28 21:11:58 -0500

    Fixed intakes names

--- StrikeZone_InfiniteRecharge/src/main/java/frc/robot/Subsystems/Intakes.java
+++ StrikeZone_InfiniteRecharge/src/main/java/frc/robot/Subsystems/Intakes.java
@@ -22,17 +22,25 @@ public class Intakes extends RobotMap{
     private boolean isIntakeIn = false;
     private boolean toggle = false;
 
-
-    public void intakesOut(){
+/**
+ * Puts the intakes out
+ */
+    public void intakesExtend(){
           //intakeIn.set(false);
         intakeOut.set(true);
     }
-
-    public void intakesIn(){
+/**
+ * Puts the intakes in
+ */
+    public void intakesRetract(){
         intakeOut.set(false);
         //intakeIn.set(true);
     }
-
+/**
+ * A toggle for the intake
+ * @param input instantaneous boolean
+ * @return the current state of the intakes (true = extended)
+ */
     public boolean intakesIO(boolean input){
         //intakes in and out (toggle)
         if(input) toggle = !toggle; 
@@ -47,14 +55,20 @@ public class Intakes extends RobotMap{
             return false;  
         }
     }
-
-    public void intakesOn(boolean bypass){
+    /**
+     * intakes balls from the field only if intakes are extended
+     * @param bypass By passes the intake out check
+     */
+    public void intakeOn(boolean bypass){
         if(!isIntakeIn || bypass){
             intake.set(ControlMode.PercentOutput, 1);
         }else{
             intake.set(ControlMode.PercentOutput, 0);
         }
     }
+    /**
+     * runs intakes backwards to clear jams and balls from robot
+     */
     public void intakeOut(){
         if(!isIntakeIn){
             intake.set(ControlMode.PercentOutput, -.5);
@@ -62,7 +76,10 @@ public class Intakes extends RobotMap{
             intake.set(ControlMode.PercentOutput, 0);
         }  
     }
-    public void intakesOff(){
+    /**
+     * turns off intakes
+     */
+    public void intakeOff(){
         if(!isIntakeIn){
             intake.set(ControlMode.PercentOutput, 0);
         }
